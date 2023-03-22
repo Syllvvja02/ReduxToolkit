63ac8a6118070a07bfb6d097833e4a1e8ee44de5
Syllvvja02 <mikolajczykdrazeksylwia@gmail.com>
2023-03-22 10:24:49 +0100

    styles css

--- RdxTool/src/App.js
+++ RdxTool/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import {Provider} from "react-redux";
 import {store} from "./BlogApp/store";
+import PostsList from "./Features/Posts/PostsList";
 
 const App = () => {
     return(
         <Provider store={store}>
-
+            <PostsList/>
         </Provider>
     )
 }
